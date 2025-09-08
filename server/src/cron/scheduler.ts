0ff8432bf8f09a5ba09afd904413c086b7391473
Ninja 903 <rushabhshah658@gmail.com>
2025-09-08 18:00:41 +0530

    changes

--- server/src/cron/scheduler.ts
+++ server/src/cron/scheduler.ts
@@ -29,6 +29,6 @@ export const startJobs = () => {
     scheduleJob("Transactions", "5 0 * * *", processRecurringTransactions),
 
     //run 2:30am every first of the month
-    scheduleJob("Reports", "*/1 * * * *", processReportJob),
+        scheduleJob("Reports", "30 2 1 * *", processReportJob),
   ];
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,62 +1,62 @@
-import "dotenv/config"
-import "./config/passport.config"
-import express, { NextFunction, Request, Response } from "express"
-import { Env } from "./config/env.config"
-import cors from "cors"
-import { HTTPSTATUS } from "./config/http.config"
-import { errorHandler } from "./middlewares/errorHandler.middleware"
-import { BadRequestException } from "./utils/app-error"
-import connctDatabase from "./config/database.config"
-import authRoutes from "./routes/auth.route"
-import passport from "passport"
-import userRoutes from "./routes/user.route"
-import { passportAuthenticateJwt } from "./config/passport.config"
+
+import "dotenv/config";
+import "./config/passport.config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import passport from "passport";
+import { Env } from "./config/env.config";
+import { HTTPSTATUS } from "./config/http.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import { BadRequestException } from "./utils/app-error";
+import { asyncHandler } from "./middlewares/asyncHandler.middlerware";
+import connctDatabase from "./config/database.config";
+import authRoutes from "./routes/auth.route";
+import { passportAuthenticateJwt } from "./config/passport.config";
+import userRoutes from "./routes/user.route";
 import transactionRoutes from "./routes/transaction.route";
-import { startJobs } from "./cron/scheduler"
-import { initializeCrons } from "./cron"
-import reportRoutes from "./routes/report.route"
-import { calulateNextReportDate } from "./utils/helper"
+import { initializeCrons } from "./cron";
+import reportRoutes from "./routes/report.route";
+import analyticsRoutes from "./routes/analytics.route";
 
-
-
-const app = express()
+const app = express();
 const BASE_PATH = Env.BASE_PATH;
-app.use(express.json())
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(passport.initialize());
 
-
 app.use(
-    cors({
-        origin: Env.FRONTEND_ORIGIN,
-        credentials: true,
-    })
+  cors({
+    origin: Env.FRONTEND_ORIGIN,
+    credentials: true,
+  })
 );
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-    throw new BadRequestException("this is test ")
+app.get(
+  "/",
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    throw new BadRequestException("This is a test error");
     res.status(HTTPSTATUS.OK).json({
-        message: "hello server is running"
-    })
-});
-
-
+      message: "Hello Subcribe to the channel",
+    });
+  })
+);
 
 app.use(`${BASE_PATH}/auth`, authRoutes);
 app.use(`${BASE_PATH}/user`, passportAuthenticateJwt, userRoutes);
 app.use(`${BASE_PATH}/transaction`, passportAuthenticateJwt, transactionRoutes);
-app.use(`${BASE_PATH}/report`, passportAuthenticateJwt, reportRoutes)
+app.use(`${BASE_PATH}/report`, passportAuthenticateJwt, reportRoutes);
+app.use(`${BASE_PATH}/analytics`, passportAuthenticateJwt, analyticsRoutes);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(Env.PORT, async () => {
-    await connctDatabase();
-    
-    
+  await connctDatabase();
+
   if (Env.NODE_ENV === "development") {
     await initializeCrons();
   }
-  
-    console.log(`Server is running on port ${Env.PORT} in ${Env.NODE_ENV} mode`);
-    
+
+  console.log(`Server is running on port ${Env.PORT} in ${Env.NODE_ENV} mode`);
 });
